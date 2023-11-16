7baf0fb19c2610da7cb0aae43f6eb108045cc5d1
Fabrizio Leali <fabrizioleali2205@gmail.com>
2023-11-15 21:28:31 -0300

    act

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ let tel_2 = document.querySelector(".telaranias-2");
 let miles = document.querySelector(".miles-morales");
 let duende = document.querySelector(".duende-verde");
 let karts = document.querySelector(".imagenes-kart");
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function(){  
     let value = window.scrollY;
 
     fondo.style.left = value * 0.3 + "px";
@@ -40,9 +40,27 @@ window.addEventListener("scroll", function(){
     gwen.style.left = - value * 0.5 + "px";
     tel_2.style.left = value * 0.1 + "px";
     miles.style.right = -value * 0.1 + "px";
-    duende.style.transform = "translate(" + 0 + ", " + 0.05 * value + "px" + ")";
+    // duende.scrollTop = duende.scrollTop/2;
     karts.style.transform = "translate(" + 0 + ", " + 0.05 * value + "px" + ")";
 })
+
+var miDiv = document.querySelector('.duende-verde');
+
+window.addEventListener('scroll', function() {
+    // Si deseas realizar la acción después de un cierto tiempo, puedes usar setTimeout
+    setTimeout(function() {
+        // Obtén el desplazamiento vertical
+        var scrollY = window.scrollY;
+
+        // Calcula la nueva posición del div 
+        var nuevaPosicion = scrollY * 0.089 + 'px';
+
+        // Aplica la nueva posición al div
+        miDiv.style.transform = "translate(" + 0 + ", " + nuevaPosicion + ")";
+        miDiv.style.transition = "smooth";
+    }, 5000); // 500 milisegundos de retardo
+});
+
 
 const sectionCards = document.querySelector(".cards-seccion");
 let sectionPeter = document.querySelector(".peter-oculto");
@@ -54,8 +72,58 @@ document.addEventListener("scroll", function () {
   const sectionCardsY = sectionCards.getBoundingClientRect().y;
   const sectionCardsHeight = sectionCards.getBoundingClientRect().height;
   if (clientHeight > sectionCardsY + (sectionCardsHeight * 2) / 3) {
-    sectionPeter.classList.add("mostrar-peter");
-    sectionGwen.classList.add("mostrar-gwen");
-    sectionMiles.classList.add("mostrar-miles");
-  }
-  });
+    setTimeout(aniadirPeter,
+                100);
+    setTimeout(aniadirGwen,
+                1000);
+    setTimeout(aniadirMiles,
+                1900);
+}});
+
+function aniadirPeter(){
+  sectionPeter.classList.add("mostrar-peter");
+}
+
+function aniadirGwen(){
+  sectionGwen.classList.add("mostrar-gwen");
+}
+
+function aniadirMiles(){
+  sectionMiles.classList.add("mostrar-miles")
+}
+
+let img_1 = document.querySelector(".img-1");
+let img_2 = document.querySelector(".img-2");
+let img_3 = document.querySelector(".img-3");
+
+
+img_1.addEventListener("mouseover", () => {
+  img_1.classList.add("img-1-agrandar");
+  console.log("hola")
+});
+img_1.addEventListener("mouseleave", () => {
+  img_1.classList.remove("img-1-agrandar");
+  console.log("chau")
+});
+
+
+
+img_2.addEventListener("mouseover", () => {
+  img_2.classList.add("img-2-agrandar");
+  console.log("hola")
+});
+img_2.addEventListener("mouseleave", () => {
+  img_2.classList.remove("img-2-agrandar");
+  console.log("chau")
+});
+
+
+
+img_3.addEventListener("mouseover", () => {
+  img_3.classList.add("img-1-agrandar");
+  console.log("hola")
+});
+img_3.addEventListener("mouseleave", () => {
+  img_3.classList.remove("img-1-agrandar");
+  console.log("chau")
+});
